6105f34c16717110ee13998e1960d4c6748f10d2
chickenmicken <luca.iacopetta@gmx.ch>
2024-04-17 15:54:27 +0200

    letzte test i hope

--- src/main/java/com/example/gitops/GitopsApplication.java
+++ src/main/java/com/example/gitops/GitopsApplication.java
@@ -1,8 +1,8 @@
 package com.example.gitops;
-
+//wtf
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//wieso
 @SpringBootApplication
 public class GitopsApplication {
 
